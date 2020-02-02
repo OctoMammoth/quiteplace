a67c38c6854fe414a65cac571071c24055c7a951
Vanya <goldenaoki@gmail.com>
2020-02-02 16:09:46 +1000

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 0.2F;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.Rotate(new Vector3(1, 0 ));
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.Rotate(new Vector3(-1, 0));
+
+        int nbTouches = Input.touchCount;
+
+        if (nbTouches > 0)
+        {
+            for (int i = 0; i < nbTouches; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+
+                TouchPhase phase = touch.phase;
+
+                switch (phase)
+                {
+                    case TouchPhase.Began:
+                        break;
+                    case TouchPhase.Moved:
+                        transform.Rotate(new Vector3(touch.deltaPosition.y *  speed, touch.deltaPosition.x * - speed));
+                        break;
+                    case TouchPhase.Ended:
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraBeh.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBeh : MonoBehaviour
+{
+    public GameObject Sphere;
+    public Material WinMat;
+    private bool quite = false;
+    public GameObject quitePlace;
+    public AudioClip quiteAudio;
+    public GameObject sprite;
+    public Sprite OK;
+
+    void natura()
+    {
+        if (quite == false) {
+            AudioSource asd = quitePlace.AddComponent<AudioSource>();
+            asd.clip = quiteAudio;
+            asd.loop = true;
+            asd.Play();
+
+        }
+        quite = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Trigger")
+        {
+            Debug.Log("WIN");
+            Sphere.GetComponent<MeshRenderer>().material = WinMat;
+            sprite.GetComponent<SpriteRenderer>().sprite = OK;
+            natura();
+        }
+    }   
+    //private void OnTriggerExit(Collider other)
+    //{
+
+    //    if (other.gameObject.tag == "Trigger")
+    //    {
+    //        Debug.Log("WIN");
+    //    }
+    //}
+}
